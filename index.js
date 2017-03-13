82463f73cde2e715750071a10b31f9e48b7f4fff
mr433 <max.rodewald@gmail.com>
2017-03-13 12:32:07 -0600

    cleaning up console.logs

--- index.js
+++ index.js
@@ -1,32 +1,30 @@
 'use strict';
 var Alexa = require('alexa-sdk');
 var rp = require('request-promise');
+
 var postData = {
-    "userid":1,
-    "textRequest":""
+    "userid": 1,
+    "textRequest": ""
 };
+
 var options = {
     url: 'https://hoops-qa.herokuapp.com/api/handlerequest',
-    method:'POST',
+    method: 'POST',
     body: postData,
-    json: true,
+    json: true
 };
 
 var handlers = {
     'GetMessage': function(){
         var message = this.event.request.intent.slots.speech.value;
-        console.log(message)
         postData.textRequest = message;
-        console.log(postData);
         rp(options, (error, response, body) => {
-            console.log(response);
-            console.log(body);
             this.emit(':tell', body.text);
         })
     }
 };
 
-exports.handler = function(event, context, callback) {
+exports.handler = function(event, context, callback){
     var alexa = Alexa.handler(event, context);
     alexa.registerHandlers(handlers);
     alexa.execute();
